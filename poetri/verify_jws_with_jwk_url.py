cf1345ebd36201a5b0b5bc8823e22eaedf54eb3d
Alan Viars <aviars@videntity.com>
2017-07-19 16:41:53 -0400

    Only fetch key file with https. Derive URL from iss claim. Enhance error handling.

--- poetri/verify_jws_with_jwk_url.py
+++ poetri/verify_jws_with_jwk_url.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python
 import sys
 import json
-from collections import OrderedDict
-from jwkest.jws import JWS
+from jwkest.jws import JWS, NoSuitableSigningKeys
+from jwkest.jwt import JWT
 from jwkest.jwk import RSAKey
 from jwkest import BadSignature
 import requests
 
+__author__ = 'Alan Viars @aviars'
 
-def verify_poet_via_url(my_jws, issuer, secure=True):
 
-    # Fetch the public key
-    
-    if secure: 
-        url = "https://%s/.well-known/poet.jwk" % (issuer)
-    else:
-        url = "http://%s/.well-known/poet.jwk" % (issuer)
-    
-    r = requests.get(url)
-    
-    if r.status_code != 200:
-        print("The key could not be fetched.")
-        exit(1)
+def verify_poet_via_url(my_jws):
 
     # load the Signed JWT (JWS)
     my_jws = my_jws.rstrip().lstrip()
     signed_token = JWS(my_jws)
+
+    # create aplain old JWT so we can fish out the 'iss'
+    t = JWT()
+    unpacked = t.unpack(token=my_jws)
+    payload = unpacked.payload()
+    if "iss" not in payload:
+        print("Missing 'iss' claim in the JWS payload.")
+        exit(1)
+    else:
+        iss = payload['iss']
+
+    # Fetch the public key from iss
+
+    url = "https://%s/.well-known/poet.jwk" % (iss)
+    r = requests.get(url)
+
+    if r.status_code != 200:
+        print("The key could not be fetched.")
+        exit(1)
 
     # load the JWK into an RSA Key structure
     rsak = RSAKey(**r.json())
@@ -34,6 +42,9 @@ def verify_poet_via_url(my_jws, issuer, secure=True):
         retval = vt
     except BadSignature:
         retval = {"error": "The signature did not match"}
+    except NoSuitableSigningKeys:
+        retval = {"error": str(sys.exc_info()[1])}
+
     except:
         retval = {"error": str(sys.exc_info())}
     return retval
@@ -41,17 +52,17 @@ def verify_poet_via_url(my_jws, issuer, secure=True):
 # command line app.
 if __name__ == "__main__":
 
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 2:
         print("Usage:")
-        print("verify_jws_with_jwk_url.py [JWT_FILE_PATH] [JWK_ISSUER]")
-        print("Example: verify_jws_with_jwk_url.py my.jwt  example.com")
+        print("verify_jws_with_jwk_url.py [JWT_FILE_PATH]")
+        print("Example: verify_jws_with_jwk_url.py my.jwt")
         sys.exit(1)
 
     my_jwt_file = sys.argv[1]
     issuer = sys.argv[2]
     jwt_fh = open(my_jwt_file)
 
-    result = verify_poet_via_url(jwt_fh.read(), issuer, secure=False)
+    result = verify_poet_via_url(jwt_fh.read())
     result = json.dumps(result, indent=4)
     jwt_fh.close()
     print(result)
